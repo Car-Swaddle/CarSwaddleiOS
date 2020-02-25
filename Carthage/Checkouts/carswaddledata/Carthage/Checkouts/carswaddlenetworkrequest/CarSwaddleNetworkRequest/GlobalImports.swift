a865490143083863065d0432d7bdfce0dbc3712c
Kyle Kendall <kkendall33@gmail.com>
2020-02-24 23:32:33 -0700

    Remove stuff

--- Carthage/Checkouts/carswaddledata/Carthage/Checkouts/carswaddlenetworkrequest/CarSwaddleNetworkRequest/GlobalImports.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GlobalImports.swift
-//  CarSwaddleNetworkRequest
-//
-//  Created by Kyle Kendall on 9/20/18.
-//  Copyright © 2018 CarSwaddle. All rights reserved.
-//
-
-@_exported import NetworkRequest
